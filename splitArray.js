ae24f503ee241725a85cb29d1b649de18f2af448
Sedorriku <73949405+Sedorikku1949@users.noreply.github.com>
2021-08-08 17:09:06 +0200

    Comment for splitArray

--- splitArray.js
+++ splitArray.js
@@ -1,5 +1,15 @@
-
-function(n) {
+/**
+  * This prototype can split a array in equal parts
+  *
+  * @param { Number } n - the length for a parts
+  *
+  * if the array is lower than n
+  * @returns { Array } Array - the array of the beginning
+  *
+  * else
+  * @returns { Array } Array - the splited array
+  */
+Array.prototype.splitArray = function(n) {
   if (typeof this !== "object" || typeof n !== "number") throw new Error(typeof this !== "object" ? "This prototype is only for Array" : "The arguments for separation must be a number"); 
   return (this.length <= n ? this : chunk(this, n)); 
 }; 
