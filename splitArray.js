144bce795c716234848b8bfb401f7809ad1b10ae
Sedorriku <73949405+Sedorikku1949@users.noreply.github.com>
2021-08-10 10:59:59 +0200

    Function forgotten

--- splitArray.js
+++ splitArray.js
@@ -9,6 +9,10 @@
   * else
   * @returns { Array } Array - the splited array
   */
+function chunk(arr, n) {
+  return (arr.length ? [arr.slice(0, n), ...chunk(arr.slice(n), n)] : []);
+};
+
 Array.prototype.splitArray = function(n) {
   if (typeof this !== "object" || typeof n !== "number") throw new Error(typeof this !== "object" ? "This prototype is only for Array" : "The arguments for separation must be a number"); 
   return (this.length <= n ? this : chunk(this, n)); 
